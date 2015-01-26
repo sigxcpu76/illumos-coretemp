3dd52c70c3d8cded697ea2bad438f82a5e6b10dc
Alexandru Pirvulescu <alex@tcnbroadcasting.com>
2015-01-26 23:51:57 +0200

    naming the module cpu_info confuses psrinfo

--- coretemp.h
+++ coretemp.h
@@ -2,7 +2,7 @@
 
 #define	__CORETEMP_H
 
-#define	KSTAT_CORETEMP_MODULE	"cpu_info"
+#define	KSTAT_CORETEMP_MODULE	"coretemp"
 #define	KSTAT_CORETEMP_NAME		"coretemp"
 #define	KSTAT_CORETEMP_CLASS	"environment"
 
